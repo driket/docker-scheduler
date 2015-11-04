d3bfd990dfe56fde107c924a33116d1661f1da4a
Cedric Villa <cedric.villa@gmail.com>
2015-11-04 16:44:16 +0100

    add curl + cron + setup cron + display logs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-#FROM ubuntu:latest
-FROM debian:wheezy
+FROM ubuntu:latest
 MAINTAINER cedsoft@zestprod.com
+
+RUN apt-get update && \
+    apt-get install -y cron curl
 
 ADD start.sh /start.sh
 RUN chmod +x /start.sh
--- start.sh
+++ start.sh
@@ -3,12 +3,23 @@
 set -e
 
 FREQUENCY=${FREQUENCY:-0 1 * * *}
-WEBSERVICE_URL=${WEBSERVICE_URL:'http://localhost'}
-METHOD=${METHOD:'GET'}
-DATA=${DATA:''}
+WEBSERVICE_URL=${WEBSERVICE_URL:-http://localhost}
+METHOD=${METHOD:-GET}
 
 echo "WEBSERVICE SCHEDULER"
 echo "Webservice URL: ${WEBSERVICE_URL}"
 echo "Frequency:      ${FREQUENCY}"
 echo "Method:         ${METHOD}"
 echo "Data:           ${DATA}"
+
+LOGFIFO='/var/log/cron.fifo'
+if [[ ! -e "$LOGFIFO" ]]; then
+    mkfifo "$LOGFIFO"
+fi
+
+CMD="curl -X ${METHOD} ${WEBSERVICE_URL}"
+
+echo -e "$FREQUENCY $CMD > $LOGFIFO 2>&1" | crontab -
+crontab -l
+cron
+tail -f "$LOGFIFO"
